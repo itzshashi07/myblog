1a9052ba40434fafdae21d97eb8a16c194e499dc
itzsh <itzshashi07@gmail.com>
2023-11-17 23:09:28 +0530

    commit changes

--- src/rightColumn/RightColumn.js
+++ src/rightColumn/RightColumn.js
@@ -5,9 +5,9 @@ import './RightColumn.css'
 function RightColumn() {
   return (
     <div className='rightColumn'>
-          <img  className='rightColumn-img' src="https://i.pinimg.com/originals/65/55/14/655514b21da89c760b3e8bd6442cb830.jpg"alt=""/>
+          <img  className='rightColumn-img' src="https://img.freepik.com/free-photo/smiling-pretty-female-model-pointing-fingers-left-empty-space-inviting-check-it-out-showing-advertisement-white-wall_176420-38810.jpg?size=626&ext=jpg"alt=""/>
 
-      <img  className='rightColumn-img' src="https://i.pinimg.com/originals/65/55/14/655514b21da89c760b3e8bd6442cb830.jpg"alt=""/>
+      {/* <img  className='rightColumn-img' src="https://img.freepik.com/free-photo/smiling-pretty-female-model-pointing-fingers-left-empty-space-inviting-check-it-out-showing-advertisement-white-wall_176420-38810.jpg?size=626&ext=jpg"alt=""/> */}
    
     
     </div>
